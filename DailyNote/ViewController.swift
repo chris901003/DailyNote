a0ba00b2a8504a478fe586b31320c4ac1b83cebb
chris901003 <a0987999103@gmail.com>
2025-07-14 19:42:42 +0800

    Initial Commit

---
+++ DailyNote/ViewController.swift
@@ -0,0 +1,20 @@
+// Created for DailyNote in 2025
+// Using Swift 6.0
+//
+//
+// Created by HongYan on 2025/7/14.
+// Copyright © 2025 HongYan. All rights reserved.
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
